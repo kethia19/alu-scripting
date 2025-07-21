b0ecfccef5328b4967641303a8cc9577c869f9e0
kethia19 <k.kayigire@alustudent.com>
2025-07-21 14:36:32 +0200

    Adding changes to 2 files

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,8 +7,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    # """Return the number of subscribers """
+    """
+    Returns the number of subscribers.
+
+    Args:
+        subreddit (str): The subreddit to query.
+
+    Returns:
+        int: The number of subscribers, or 0 if invalid.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
