5a3cf8833fe63dc717b96b2d22a71c9adae08539
kethia19 <k.kayigire@alustudent.com>
2025-07-21 14:45:02 +0200

    Adding changes to 1 file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Module defines a function to query the Reddit API and print
-the titles of the top 10 hot posts.
+This module defines a function to query the Reddit API and print
+the titles of the top 10 hot posts for a given subreddit.
 If the subreddit is invalid or inaccessible, it prints "OK".
 """
 
@@ -9,10 +9,10 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print top 10 hot posts or OK if invalid"""
+    """Print top 10 hot post titles or 'OK' if subreddit is invalid."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'python:subreddit.hot.posts:v1.0'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
