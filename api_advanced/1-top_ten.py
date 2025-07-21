57a00f166fd7ea31645341335116280adce33716
kethia19 <k.kayigire@alustudent.com>
2025-07-21 14:55:24 +0200

    Adding changes to 2 files

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
-"""
-This module defines a function to query the Reddit API and print
-the titles of the top 10 hot posts for a given subreddit.
-"""
-
+"""DOCS"""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a given subreddit.
+    """Prints the titles of the first 10 hot posts"""
+    url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
 
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    If the subreddit is invalid or inaccessible, prints 'None'.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'python:subreddit.hot.posts:v1.0'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            for post in posts[:10]:
-                print(post.get('data', {}).get('title'))
-            return
-    except Exception:
+    if response.status_code == 200:
+        posts = response.json()['data']
+        for post in posts['children'][:10]:
+            print(post['data']['title'])
+    else:
         print(None)
-    print("OK")
