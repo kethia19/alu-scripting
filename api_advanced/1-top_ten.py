fa1faecec3e657d491287abc9f23b88b318e2689
kethia19 <k.kayigire@alustudent.com>
2025-07-22 10:26:31 +0200

    Last Changes on 1 file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """DOCS"""
 import requests
-import sys
 
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
     headers = {'User-agent': 'Mozilla/5.0'}
-    
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             posts = response.json()['data']
             for post in posts['children'][:10]:
-                sys.stdout.write(post['data']['title'] + '\n')
+                print(post['data']['title'])
             return
     except Exception:
         pass
-    
-    sys.stdout.write("OK")
+    print("OK", end="")
