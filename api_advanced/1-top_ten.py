d474883fb209c4775436d7f02274f27771d5fc10
kethia19 <k.kayigire@alustudent.com>
2025-08-04 23:27:31 +0200

    Adding new changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""DOCS"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts"""
-    url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = {'User-agent': 'Mozilla/5.0'}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            posts = response.json()['data']
-            for post in posts['children'][:10]:
-                print(post['data']['title'])
-            return
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
-        pass
-    print("OK", end="")
+        print(None)
